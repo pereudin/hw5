ad76bac7cbc7d449f7acc639b197f19172f09cce
pereudin <a.a.pereudin@gmail.com>
2019-10-22 22:17:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/pereudin/hw5/RoomActivity.java
@@ -0,0 +1,14 @@
+package ru.pereudin.hw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+}
